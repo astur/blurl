714031146c625cab0b3d78460e7e760315a6f1af
astur <astur@yandex.ru>
2019-10-18 20:21:02 +0300

    base test improved

--- test.js
+++ test.js
@@ -5,6 +5,10 @@ test('base', t => {
     t.is(blurl('not-site'), 'not-site');
     t.is(blurl('not-http://www.site.ru'), 'not-http://www.site.ru');
     t.is(blurl('http://www.site.ru'), 'http://www.site.ru');
+    t.is(blurl('http://www.site.ru/'), 'http://www.site.ru/');
+    t.is(blurl('https://www.site.ru/'), 'https://www.site.ru/');
+    t.is(blurl('http://www.site.ru?a=1'), 'http://www.site.ru?a=1');
+    t.is(blurl('http://www.site.ru/?a=1'), 'http://www.site.ru/?a=1');
     t.is(blurl('http://user@www.site.ru'), 'http://user@www.site.ru');
     t.is(blurl('http://www.site.ru/path/to/dir?a=1&b=2#link'), 'http://www.site.ru/path/to/dir?a=1&b=2#link');
 });
