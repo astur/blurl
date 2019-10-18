3a98c7df40d9cec21b2457e7758f467742f54dd2
astur <astur@yandex.ru>
2019-10-18 19:52:19 +0300

    base test

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 const test = require('ava');
-const m = require('.');
+const blurl = require('.');
 
-test('blurl', t => {
-    t.true(true);
-    t.is(m, m);
+test('base', t => {
+    t.is(blurl('not-site'), 'not-site');
+    t.is(blurl('not-http://www.site.ru'), 'not-http://www.site.ru');
+    t.is(blurl('http://www.site.ru'), 'http://www.site.ru');
+    t.is(blurl('http://user@www.site.ru'), 'http://user@www.site.ru');
+    t.is(blurl('http://www.site.ru/path/to/dir?a=1&b=2#link'), 'http://www.site.ru/path/to/dir?a=1&b=2#link');
 });
