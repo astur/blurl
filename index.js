63b8aab8b83da43b5b833c4f23b6d82984b7cc49
astur <astur@yandex.ru>
2019-12-23 15:50:27 +0300

    blurPassword array option

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 module.exports = (url, {blurPassword = true} = {}) => url
     .replace(
         /http(s?):\/\/([^:.@/]+):([^:.@/]+)@/,
-        (match, p1, p2, p3) => blurPassword ? `http${p1}://${p2}:${p3.split('').map(v => '*').join('')}@` : match
+        (match, p1, p2, p3) => {
+            if(Array.isArray(blurPassword)){
+                const pArr = p3.split('');
+                const [i, j] = blurPassword;
+                if(i + j > pArr.length) return match;
+                const n = pArr.length - i - j;
+                pArr.splice(i, n, '*'.repeat(n));
+                return `http${p1}://${p2}:${pArr.join('')}@`;
+            }
+            return blurPassword ? `http${p1}://${p2}:${p3.split('').map(v => '*').join('')}@` : match;
+        }
     );
